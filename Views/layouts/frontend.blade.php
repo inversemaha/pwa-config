5a71992076393881dc5fa01022e36bfaf9115468
nickgglink <nick.gglink@gmail.com>
2022-08-31 14:03:42 +0600

    Initilized

---
+++ Views/layouts/frontend.blade.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en" itemscope itemtype="http://schema.org/WebPage">
+<head>
+    <!-- Meta Tags-->
+    <meta charset="utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Tommy | Elevate your business to the next level</title>
+    
+    <!--Search Engine Optimization Meta Tags -->
+    <meta name="title" content="Tommy | Elevate your business to the next level">
+    <meta name="description" content="Tommy is a web-based CRM software that will help operate your business efficiently. If you are looking for easy-to-use and affordable CRM software, then Tommy is the right choice for you.">
+    <meta name="keywords" content="CRM system, CRM software, businesses with CRM, advantages of CRM, smooth transition to CRM, manage leads, support manager, software for any businesses, affordable software, easy to use software, move to CRM, CRM for everyone">
+    <meta name="robots" content="index,follow">
+    <meta name="revisit-after" content="7 days">
+    <meta name="distribution" content="web">
+    <meta name="robots" content="tommy">
+
+    <!--Optional Meta Tags -->
+    <meta name="distribution" content="web">
+    <meta name="web_author" content="Adam Levine">
+    <meta name="rating" content="100/100 Google Page insight">
+    <meta name="rating" content="100/100 Google Schema">
+    <meta name="subject" content="Corporate">
+    <meta name="copyright" content="Copyright © Tommy 2022. All Rights Reserved.">
+    <meta name="reply-to" content="email">
+    <meta name="city" content="London">
+    <meta name="country" content="United Kingdom">
+    <meta name="distribution" content="global">
+
+    <!--OG (Open Graph) Meta -->
+    <meta property="og:title" content="Tommy | Elevate your business to the next level">
+    <meta property="og:description" content="Tommy is a web-based CRM software that will help operate your business efficiently. If you are looking for easy-to-use and affordable CRM software, then Tommy is the right choice for you.">
+    <meta property="og:locale" content="en-GB"/>
+    <meta property="og:image" content="https://tommy.ggtasker.co.uk/assets/images/branding/socialShare/tommy-social-share.png">
+    <meta property="og:image:width" content="1200"/>
+    <meta property="og:image:height" content="630"/>
+    <meta property="og:url" content="https://tommy.ggtasker.co.uk/"/>
+
+    <!-- Facebooks Cards  -->
+    <meta name="twitter:card" content="https://ggtasker.co.uk/assets/images/projects/tommy/socialshareicon.png"/>
+    <meta property="facebook:title" content="Tommy | Elevate your business to the next level"/>
+    <meta property="facebook:description" content="Tommy is a web-based CRM software that will help operate your business efficiently. If you are looking for easy-to-use and affordable CRM software, then Tommy is the right choice for you."/>
+    <meta property="facebook:locale" content="en_GB"/>
+    <meta property="facebook:image" content="https://tommy.ggtasker.co.uk/assets/images/branding/socialShare/tommy-social-share.png"/>
+    <meta property="facebook:url" content="https://www.facebook.com/Tommy-104328658788547"/>
+
+    <!-- Twitter Cards  -->
+    <meta name="twitter:card" content="https://ggtasker.co.uk/assets/images/projects/tommy/socialshareicon.png"/>
+    <meta property="twitter:title" content="Tommy | Elevate your business to the next level"/>
+    <meta property="twitter:description" content="Tommy is a web-based CRM software that will help operate your business efficiently. If you are looking for easy-to-use and affordable CRM software, then Tommy is the right choice for you."/>
+    <meta property="twitter:locale" content="en_GB"/>
+    <meta property="twitter:image" content="https://tommy.ggtasker.co.uk/assets/images/branding/socialShare/tommy-social-share.png"/>
+    <meta property="twitter:url" content="https://twitter.com/Tommy630034641"/>
+    
+    <!-- Linkedin Cards  -->
+    <meta name="linkedin:card" content="https://ggtasker.co.uk/assets/images/projects/tommy/socialshareicon.png"/>
+    <meta property="linkedin:title" content="Tommy | Elevate your business to the next level"/>
+    <meta property="linkedin:description" content="Tommy is a web-based CRM software that will help operate your business efficiently. If you are looking for easy-to-use and affordable CRM software, then Tommy is the right choice for you."/>
+    <meta property="linkedin:image" content="https://tommy.ggtasker.co.uk/assets/images/branding/socialShare/tommy-social-share.png"/>
+    <meta property="linkedin:url" content="https://www.linkedin.com/company/tommyuk/?viewAsMember=true"/>
+
+    <!--touch-icon -->
+    <link rel="icon" type="image/png" sizes="16x16" href="/assets/images/branding/favicon/tommy-favicon-16x16.png">
+    <link rel="apple-touch-icon" type="image/png" sizes="32x32" href="/assets/images/branding/favicon/apple-icon-120x120.png">
+    
+     <!--favicons -->
+    <link href="{{ asset('/assets/images/branding/favicon/favicon.png') }}" rel="icon" type="image/png" />
+    
+    <link href="/assets/images/branding/logo/favicon.png" rel="icon" sizes="16x16" type="image/png" />
+    <link href="/assets/images/branding/logo/favicon.png" rel="shortcut icon" sizes="16x16" type="image/x-icon" />
+
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="/assets/images/branding/favicon/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+    <!--web-manifest -->
+    <link rel="manifest" href="/assets/images/branding/favicon/manifest.json">
+    
+</head>
+<body>
+
+
+<!-- Call to run serviceWorker.js file-->
+<script data-cfasync="false" src="/assets/scripts/app.js"></script>
+
+</body>
+</html>
